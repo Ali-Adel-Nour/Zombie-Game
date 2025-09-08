6be16370d982e91bc91e4752c5686ca05fec851d
a.kenawy@v4tech.com <a.kenawy@v4tech.com>
2025-09-08 19:34:37 +0300

    Enhancmet ZombieFeeding

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -1,7 +1,5 @@
-pragma solidity ^0.8.19;
-
-import "./ZombieFactory.sol";
-
+pragma solidity >=0.5.0 <0.6.0;
+import "./zombiefactory.sol";
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -16,13 +14,26 @@ contract KittyInterface {
     uint256 genes
   );
 }
-
 contract ZombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-        require(_zombieId < zombies.length, "Invalid zombie ID");
-        Zombie storage myZombie = zombies[_zombieId];
-        _targetDna = _targetDna % dnaModulus;
-        uint newDna = (myZombie.dna + _targetDna) / 2;
-        _createZombie("NoName", newDna);
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
     }
+    _createZombie("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
 }
